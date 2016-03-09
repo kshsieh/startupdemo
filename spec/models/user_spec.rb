78c9bb5063be21ac85883be27d873d65dc8eae51
Kevin Hsieh <kshsieh@wework.com>
2016-03-09 12:31:44 -0500

    define User factory. first run at User spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 describe User, type: :model do
+  let(:name) { Faker::Name.name }
+  let(:phone_number) { Faker::PhoneNumber.phone_number }
+  let(:user) { FactoryGirl.create(:user, name: name, phone_number: phone_number) }
+
+  describe "some stupid tests for now" do
+    it 'returns the phone number' do
+      expect(user.name).to eq(name)
+    end
+
+    it 'returns the user name' do
+      expect(user.phone_number).to eq(phone_number)
+    end
+  end
 end
